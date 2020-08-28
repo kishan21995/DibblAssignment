f43a791f3fadbfd5fd3414a079021c8853e7a719
Kishan <kishangupta21995@gmail.com>
2020-08-27 23:39:49 -0400

    add default img

--- app/src/main/java/com/app/dibblassignment/adapter/NotificationAdapter.java
+++ app/src/main/java/com/app/dibblassignment/adapter/NotificationAdapter.java
@@ -1,6 +1,7 @@
 package com.app.dibblassignment.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
@@ -39,35 +40,71 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     @Override
     public void onBindViewHolder(@NonNull final NotificationAdapter.MyViewHolder holder, int position) {
 
-        Notification notification = notificationList.get(position);
+        final Notification notification = notificationList.get(position);
         holder.titleSong.setText(notification.getNotificationMsg());
 
 
         if(notification.getNotificationType().equalsIgnoreCase("song-suggest")){
             Picasso.with(context).load("http://13.127.198.116/storage/"+notification.getSong().getSongImage()).error(R.drawable.music).into(holder.songIMG);
-
             holder.profileIMG.setVisibility(View.GONE);
             holder.songIMG.setVisibility(View.VISIBLE);
 
-        }else{
-           // .setVisibility(View.VISIBLE);
-            holder.titleSong.setText(notification.getNotificationMsg());
-       Picasso.with(context).load("http://13.127.198.116/storage/"+notification.getUser().getImage()).error(R.drawable.music).into(holder.profileIMG);
+            holder.playBTN.setVisibility(View.VISIBLE);
+            holder.likeIMG.setVisibility(View.VISIBLE);
 
-            /*holder.profileIMG.setVisibility(View.VISIBLE);
-            holder.songIMG.setVisibility(View.GONE);*/
+
+
+
+
+            holder.likeIMG.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (notification.getNotificationType().equalsIgnoreCase("0")) {
+                       // holder.likeIMG.setVideoPath(challengeDetails.getVideo());
+
+                        holder.likeIMG.setBackgroundResource(R.drawable.unlikeimg);
+                        holder.unlikeIMG.setVisibility(View.VISIBLE);
+                        holder.likeIMG.setVisibility(View.GONE);
+
+
+                    } else {
+                        holder.likeIMG.setBackgroundResource(R.drawable.likeimg);
+                        holder.unlikeIMG.setVisibility(View.GONE);
+                        holder.likeIMG.setVisibility(View.VISIBLE);
+
+
+                    }
+
+                }
+            });
+
+
+
+
+
+
+
+
+        }else{
+
+            holder.titleSong.setText(notification.getNotificationMsg());
+            Picasso.with(context).load("http://13.127.198.116/storage/"+notification.getUser().getImage()).error(R.drawable.music).into(holder.profileIMG);
+
+
 
             holder.songIMG.setVisibility(View.GONE);
+
+            holder.playBTN.setVisibility(View.GONE);
+            holder.playBTN.setVisibility(View.GONE);
+
+
 
 
 
 
 
         }
-    /*    //holder.titleSong.setText(artist.get);
 
-        //Picasso.with(context).load(artist.get).error(R.drawable.pops).into(holder.profileIMG);
-        // Picasso.with(context).load(artist.getImage()).error(R.drawable.music).into(holder.imageView);*/
 
 
 
@@ -91,25 +128,21 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
         TextView titleSong,hours,textViewNotification;
         CardView songItem;
 
-        ImageView profileIMG,likeIMG,notification,songIMG ;
+        ImageView profileIMG,likeIMG,notification,songIMG,playBTN,unlikeIMG ;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             profileIMG = itemView.findViewById(R.id.profileIMG);
-           //songItem = itemView.findViewById(R.id.songItem);
             songIMG = itemView.findViewById(R.id.songIMG);
+            playBTN = itemView.findViewById(R.id.playBTN);
             titleSong = itemView.findViewById(R.id.title);
             likeIMG = itemView.findViewById(R.id.likeIMG);
+            unlikeIMG = itemView.findViewById(R.id.unlikeIMG);
             hours = itemView.findViewById(R.id.hour);
 
+            /*songItem = itemView.findViewById(R.id.songItem);*/
 
-
-
-
-
-
-
-        }
+ }
     }
 
 }
